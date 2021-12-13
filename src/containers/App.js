c9f6e09bd62742bce6ef9d0f6ddac046a44a3457
Sara Bermudez <bermudezalvarezsaraestefania@gmail.com>
2021-12-13 08:53:21 -0500

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Memorize from "../components/memorizacion/Memorize";
+
+function App() {
+  return (
+    <div className="App">
+      <Memorize />
+    </div>
+  );
+}
+
+export default App;
